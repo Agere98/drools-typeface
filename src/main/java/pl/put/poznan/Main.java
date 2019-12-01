55cc3ee1ad0c346682b493731bbd9d72b4ddf5cb
Agere <bmateusz1998@gmail.com>
2019-12-02 00:32:49 +0100

    Initial commit

---
+++ src/main/java/pl/put/poznan/Main.java
@@ -0,0 +1,19 @@
+package pl.put.poznan;
+
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class Main {
+
+    public static void main(final String[] args) {
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kContainer = kieServices.getKieClasspathContainer();
+        KieBase kieBase = kContainer.getKieBase();
+        KieSession session = kieBase.newKieSession();
+
+        session.fireAllRules();
+        session.dispose();
+    }
+}
