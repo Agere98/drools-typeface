f60f4f9087c3f88ecebce637a7c60b8370b32307
Vlad <hl.vladyslav@gmail.com>
2019-12-06 08:45:37 +0100

    strict order, but bad reasoning

--- src/main/java/pl/put/poznan/Main.java
+++ src/main/java/pl/put/poznan/Main.java
@@ -12,6 +12,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
